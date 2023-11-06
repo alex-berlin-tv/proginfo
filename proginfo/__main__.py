96afa50af5c2f4eb2d0725f2ae061d7b7fa153f5
alex-berlin-tv <hostettler@alex-berlin.de>
2023-11-06 16:58:36 +0100

    flags for additional control added

--- proginfo/__main__.py
+++ proginfo/__main__.py
@@ -1,25 +1,40 @@
 from .config import settings
 from .data import Formatter
+from .log import logger
+
+import argparse
 
 from omniapy import Omnia, StreamType
 
 
 def app():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tv", action="store_true", help="update tv")
+    parser.add_argument("--radio", action="store_true", help="update radio")
+    args = parser.parse_args()
+
+    if not args.tv and not args.radio:
+        logger.error("whether tv nor radio update requested by flags")
+
     formatter = Formatter()
     omnia = Omnia(settings.domain_id, settings.api_secret, settings.session_id)
-    omnia.update(
-        StreamType.RADIO,
-        settings.radio_id,
-        {
-            "title": formatter.radio_title(),
-            "description": formatter.radio_description(),
-        }
-    )
-    print(formatter.tv_description())
-    omnia.update(
-        StreamType.LIVE,
-        settings.tv_id,
-        {
-            "description": formatter.tv_description(),
-        }
-    )
+
+    if args.tv:
+        logger.info("update TV")
+        omnia.update(
+            StreamType.LIVE,
+            settings.tv_id,
+            {
+                "description": formatter.tv_description(),
+            }
+        )
+    if args.radio:
+        logger.info("update radio")
+        omnia.update(
+            StreamType.RADIO,
+            settings.radio_id,
+            {
+                "title": formatter.radio_title(),
+                "description": formatter.radio_description(),
+            }
+        )
