5ac6eb8c8f6f641e98ecd08cc3c34fc864df898b
alex-berlin-tv <hostettler@alex-berlin.de>
2023-11-07 14:28:14 +0100

    log with date and time

--- proginfo/log.py
+++ proginfo/log.py
@@ -3,6 +3,9 @@ from .config import settings
 import logging
 
 
-logging.basicConfig(level=str(settings.log_level).upper())
+logging.basicConfig(
+    level=str(settings.log_level).upper(), 
+    format="%(asctime)s;%(levelname)s:%(message)s",
+)
 
 logger = logging.getLogger()
